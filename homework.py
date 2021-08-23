17fc93017da29191ff57cac381c469e9fd0cdda5
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-23 16:06:41 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -8,6 +8,15 @@ while i<10:
  result_list.insert(i, el)
  i=i+1
 
+ el1=0
+ el2=0
+ while i<10:
+    i=i+2
+ el1=result_list([i])
+ e2=result_list({i+1})
+ result_list.insert([i,el2])
+result_list.insert([i+1,el1])
+
 
 
 
