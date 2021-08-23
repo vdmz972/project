95c93a2b1102988392e8430ade677f9203156057
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-23 16:13:21 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -3,19 +3,20 @@ i=0
 el=0
 result_list=[]
 while i<10:
- print("введите:", i, "-й эелемент ")
- el=input(i)
- result_list.insert(i, el)
- i=i+1
+  print("введите:", i, "-й эелемент ")
+  el=input(i)
+  result_list.insert(i, el)
+  i=i+1
 
- el1=0
- el2=0
- while i<10:
-    i=i+2
+el1=0
+el2=0
+while i<10:
+ i=i+2
  el1=result_list([i])
  e2=result_list({i+1})
  result_list.insert([i,el2])
-result_list.insert([i+1,el1])
+ result_list.insert([i+1,el1])
+
 
 
 
