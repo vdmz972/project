b40dda8461d2e7fbba376caa206a2dbb122f1219
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-23 16:27:46 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -13,7 +13,10 @@ el2=0
 while i<10:
  i=i+2
  el1=result_list([i])
- e2=result_list({i+1})
+ el2=result_list({i+1})
+ if el2 == False:
+  break
+
  result_list.insert([i,el2])
  result_list.insert([i+1,el1])
 
