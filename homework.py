43fbd4c16c195b3b13b2cb61d8891f2f79ea63cb
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-23 14:46:26 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,43 +1,9 @@
 #1
-a=8
-b=9
-num=in
-x=chislo//10
-while x>0:
-    x=chislo//10
-    y=chislo%10
-    if z<y:
-        z=y
-
-        print('maximum number is',z)
-
-#5
-vir=int(input('input viruchka'))
-isder=int(input('input isdergki'))
-sootn=0
-prib=0
-if vir>isder:
-    sootn=vir-isder
-    prib=sootn/vir
-    print('вы работаете с прибылью c рентабельностью',sootn)
-
-else:
-    print('вы работаете в убыток')
-chislen=int(input('сколько в фирме сотрудников'))
-prib=chislen/prib
-print('прибыль на одного сотрудника ',prib)
-
-#6
-
-a=input('введите',a)
-rez=a
-schet=0
-b=input('введите',b)
-while rez>=b:
-    rez=rez+rez/10
-    schet=schet+1
-
-print(f'результат достигнут на {schet}:день')
+result_list = [2, 'text', 456, 45.3, None]
+i=0
+while i<5:
+ print(type(result_list[i]))
+ i=i+1
 
 
 
