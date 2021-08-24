1d0022454bb03fd3f5db7693e07c43c888a59318
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-24 16:34:47 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,14 +1,24 @@
 #4
 stroka=str(input("Введите строку"))
+
+
+
 razd=stroka.split(' ')
+
+
+
 schet=len(razd)
 i=0
+a=0
 while schet>0:
-  print(i+1,razd[i])
-  i=i+1
-  schet=schet-1
-
-
+    a = razd[i]
+    print(a)
+if len(razd[i])<10:
+     print(i+1,razd[i])
+else:
+     print(a[1:10])
+i=i+1
+schet=schet-1
 
 
 
