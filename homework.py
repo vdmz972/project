f24ca48a8f35e58d9e5bff000ae4f499a8f92604
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-27 10:37:21 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,29 +1,20 @@
 #2
 a=["имя", "фамилия", "год рождения", "город проживания", "email", "телефон"]
 i=0
-b=0
-z=0
+b=[]
+z=''
 
 while i<6:
- z=input(a[i])
- b={a[i]:z[i]}
+ z=str(input (a[i]))
+ b.insert(i, z)
  i=i+1
 
-print(z)
-
-
-a=["имя", "фамилия", "год рождения", "город проживания", "email", "телефон"]
-
-global c,b,i
-c=0
 i=0
-b=''
 while i<6:
- key=input ('введите')
- b[i]=key
- i=i+1
+ print(a[i],':',b[i])
+ i+=1
 
-print(b)
+
 
 
 
