ca0ece43fd567eb8118b96a22435494d927c561e
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-27 11:47:02 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,4 +1,32 @@
 #2
+def my_sum(arg1,arg2):
+ return arg1,arg2
+a=["имя", "фамилия", "год рождения", "город проживания", "email", "телефон"]
+i=0
+b=[]
+z=''
+
+while i<6:
+ z=str(input (a[i]))
+ b.insert(i, z)
+ i=i+1
+
+'''i=0
+while i<6:
+ print(a[i],':',b[i])
+ i+=1
+
+'''
+i=0
+
+while i<6:
+ arg1=a[i]
+ arg2=b[i]
+ print({arg1,arg2})
+ i+=1
+
+
+my_sum(1,2)
 
 
 
