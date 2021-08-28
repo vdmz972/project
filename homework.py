0e53b60cac5f0750be6b539e36144bb9794ad42a
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-28 14:58:57 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,5 +1,14 @@
 #6
 
+def int_func():
+    for word in input("Введите слово из маленьких латинских букв:\n").split():
+        chars=0
+        for char in word:
+            if 97<=ord(char)<=122:
+                chars+=1
+                print(word.title()if chars==len(word) else f"{word}Только маленькие английские буквы")
+
+                int_func()
 
 
 
