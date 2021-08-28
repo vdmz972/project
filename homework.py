39193173325b8bcc41f288aa832a48762a46d620
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-28 10:50:17 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,17 +1,10 @@
 #3
 
-el=int(input('номер месяца'))
-
-seasons_list=['december','january','february','march','april','may','june','julay','august','september','october','november']
-month=(seasons_list[el])
-
-my_dict = {7:"summer", 8:"summer", 9:"summer",10:"autumn",
-           11:"autumn",12:"autumn",1:"winter",2:"winter",3:"winter",4:"spring",5:"spring",6:"spring"
-           }
-print(el)
-season=my_dict.setdefault(el)
-
-print(f"месяц:{month},время года:{season}")
-
-
-
+def my_func(num1,num2,num3):
+    my_list=[num1,num2,num3]
+    try:
+        my_list.remove(min(my_list))
+        return sum(my_list)
+    except TypeError:
+        return "Только цифры"
+    print(my_func(6,8,11))
