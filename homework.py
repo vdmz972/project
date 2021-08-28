7a86332ddd59a7bb190195b68b524169e9127a3b
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-28 11:06:28 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,26 +1,10 @@
 #4
-stroka=str(input("Введите строку"))
-
-
-
-razd=stroka.split(' ')
-
-
-
-schet=len(razd)
-i=0
-a=0
-while schet>0:
-    a = razd[i]
-
-    if len(razd[i])<10:
-     print(i+1,razd[i])
-     i = i + 1
-     schet = schet - 1
-    else:
-     print(i+1,a[1:10])
-     i=i+1
-     schet=schet-1
-
+def my_func(x,y):
+ try:
+    res=x**y
+ except TypeError:
+    return  "Положительное х и целое отрицательное у"
+ return res
+ print(my_func(3,4,-1))
 
 
