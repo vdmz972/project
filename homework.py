d2feba328205efac67b909fbf2946bd26293fe79
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-30 18:40:43 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -3,13 +3,17 @@
 my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 new_list = []
 i = 0
-for n in my_list:
-     i += 1
-     new_list.append[max(my_list[i],my_list[i+1])]
-
-
+for I in my_list:
+     
+     
+new_list = [el for el in my_list if my_list[i+1]>my_list[i]]
+I+=1
 
 print(new_list)
+
+
+
+
 
 
 
