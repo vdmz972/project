78a98f3b741906feb7ab5c2f32e681065bc038e4
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-30 16:31:44 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,19 +1,15 @@
 #2
 
 my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-
-
 new_list = []
 i = 0
-n=0
 for n in my_list:
      i += 1
-     new_list.append[max(n[i],n[i+1])]
+     new_list.append[max(my_list[i],my_list[i+1])]
 
 
 
 print(new_list)
-
 
 
 
