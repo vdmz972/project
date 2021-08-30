6937e9f6ee00a8ba7d3e8f325f6ed656844ee108
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-30 14:54:41 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,19 +1,12 @@
 #1
-def s_calc():
+#!/usr/bin/python
 
-    try:
-        r_val = int(input("Первое число "))
-        h_val = int(input("Второе число "))
-        delen=r_val/h_val
-    except ValueError:
-        return
-    s_side = 2 * 3.14 * r_val * h_val
-    s_circle = 3.14 * r_val ** 2
-    s_full = s_side + 2 * s_circle
-    return delen
-
-delen = s_calc()
-print(f"Ответ {delen}")
+from sys import argv
+script_name, vir, stavk, prem = argv
+def zarpl(vir,stavk,prem):
+    return a
+a= int(vir)*int(stavk)+int(prem)
+print(f" Ваша зарплата составляет {a} рублей")
 
 
 
