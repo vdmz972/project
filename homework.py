edaa9131ec483aebb7bbe1ef88d26a5573d92456
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-31 12:01:25 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,10 +1,4 @@
 #3
 
-def my_func(num1,num2,num3):
-    my_list=[num1,num2,num3]
-    try:
-        my_list.remove(min(my_list))
-        return sum(my_list)
-    except TypeError:
-        return "Только цифры"
-    print(my_func(6,8,11))
+my_dict = [el for el in range(20, 240) if el%21==0 or el%20==0 ]
+print(my_dict)
