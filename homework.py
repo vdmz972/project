672836da658052abc905ba71b0875b755223fd11
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-08-31 16:24:13 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,10 +1,13 @@
 #4
+import random
 my_list=[2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
 len_my_list=(len(my_list))
 new_list=[]
 i=0
 p=0
+my_dict = dict()
+
 l=set()
 while i<=13:
   l.add(my_list[i])
