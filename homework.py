aec3582ffdd727b76df090e79e3e88d58d79b34c
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-01 08:39:15 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,16 +1,10 @@
 #6
 
-def int_func():
-    for word in input("Введите слово из маленьких латинских букв:\n").split():
-        chars=0
-        for char in word:
-            if 97<=ord(char)<=122:
-                chars+=1
-                print(word.title()if chars==len(word) else f"{word}Только маленькие английские буквы")
-
-       a=int_func()
-print(a)        
-
-
+from  itertools import count,cycle
+my_count=count(7)
+my_cyrcle=cycle("abc")
+for _ in range(5):
+    print("(my_count,my_cyrcle)=({},{})".format(next(my_count),next(my_cyrcle)))
+    
 
 
