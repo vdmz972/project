c6bac29c3d7ace1a7844ce8da29b5965cab76a22
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-02 14:58:32 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,3 +1,12 @@
 #3
 
-print([el for el in range(20, 240) if el%21==0 or el%20==0 ])
+with open('sal.txt', 'r') as my_file:
+    sal = []
+    poor = []
+    my_list = my_file.read().split('\n')
+    for i in my_list:
+        i = i.split()
+        if int(i[1]) < 20000:
+           poor.append(i[0])
+        sal.append(i[1])
+print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(int, sal)) / len(sal)}')
