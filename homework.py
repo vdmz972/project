8c0a79594c26565d486878a53694924c29d0a7b8
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-02 15:00:00 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,10 +1,12 @@
 #6
 
-from  itertools import count,cycle
-my_count=count(7)
-my_cyrcle=cycle("abc")
-for _ in range(5):
-    print("(my_count,my_cyrcle)=({},{})".format(next(my_count),next(my_cyrcle)))
-    
+#import json
+
+subj = {}
+with open('file_6.txt', 'r') as init_f:
+    for line in init_f:
+        subject, lecture, practice, lab = line.split()
+        subj[subject] = int(lecture) + int(practice) + int(lab)
+    print(f'Общее количество часов по предмету - \n {subj}')
 
 
