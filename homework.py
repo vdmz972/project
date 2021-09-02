8f7be27986ec446a4e914caa4f9e09de283acc91
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-02 15:16:41 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,21 +1,20 @@
 #2
 
-my_file = open('file_2.txt', 'r')
+my_file = open('text.txt', 'r')
 content = my_file.read()
 print(f'Содержимое файла: \n {content}')
-my_file = open('file_2.txt', 'r')
+my_file = open('text.txt', 'r')
 content = my_file.readlines()
-print(f'Количество строк в файле - {len(content)}')
-my_file = open('file_2.txt', 'r')
+print(f'в файле - {len(content)} строк')
+my_file = open('text.txt', 'r')
 content = my_file.readlines()
 for i in range(len(content)):
-    print(f'Окличество символов {i + 1} - ой строки {len(content[i])}')
-my_file = open('file_2.txt', 'r')
+    print(f'В {i + 1} - ой строке {len(content[i])} символов')
+my_file = open('text.txt', 'r')
 content = my_file.read()
 content = content.split()
-print(f'Общее количество слов - {len(content)}')
+print(f'Всего слов - {len(content)}')
 my_file.close()
-
 
 
 
