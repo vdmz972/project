f24ae83f071e65c8b6ec11aa7da71fd07b8290b4
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-03 14:56:19 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,13 +1,13 @@
 #4
 rus = {'One' : 'Один', 'Two' : 'Два', 'Three' : 'Три', 'Four' : 'Четыре'}
 new_file = []
-with open('file_4.txt', 'r') as file_obj:
-    content = file_obj.read().split('\n')
+with open('text.txt', 'r') as file_obj:
+   
+
     for i in file_obj:
         i = i.split(' ', 1)
         new_file.append(rus[i[0]] + '  ' + i[1])
     print(new_file)
 
-with open('file_4_new.txt', 'w') as file_obj_2:
-    file_obj_2.writelines(new_file)
-
+    with open('new_file.txt', 'w') as file_obj_2:
+        file_obj_2.writelines(new_file)
