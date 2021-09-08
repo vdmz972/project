29e03ca180d9da51bd955c5c968cdf6cd8f0fdd6
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-08 08:02:10 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -19,6 +19,5 @@ class Position(Worker):
     def get_total_income(self):
         return self._income["wage"] + self._income["bonus"]
 
-
 p = Position('Misha', 'Mishutka', 'QA', '100000', '20000')
 print(p.get_full_name(), p.get_total_income())
