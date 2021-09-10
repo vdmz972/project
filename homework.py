276c3377dda43a1f98a469eb31b4fb7d10189d65
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-10 08:15:30 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,23 +1,34 @@
 #3
 
-class Worker:
+class Cell:
+    def __init__(self, quantity):
+        self.quantity = int(quantity)
 
-    def __init__(self, name, surname, position, wage, bonus):
-        self.name = name
-        self.surname = surname
-        self.position = position
-        self._income = {"wage": int(wage), "bonus": int(bonus)}
+    def __add__(self, other):
+        return f'Две клетки - хорошо, а одна большая - лучше! Размер клетки равен: {self.quantity + other.quantity}'
+
+    def __sub__(self, other):
+        sub = self.quantity - other.quantity
+        return f'Клеточка стала меньше, теперь она равна: {sub} клеточкам' if sub > 0 else 'Клетка исчезла :('
+
+    def __truediv__(self, other):
+        return self.quantity // other.quantity
+
+    def __mul__(self, other):
+        return self.quantity * other.quantity
+
+    def make_order(self, row):
+        result = ''
+        for i in range(int(self.quantity / row)):
+            result += '*' * row + '\n'
+        result += '*' * (self.quantity % row) + '\n'
+        return result
 
 
-class Position(Worker):
-    def __init__(self, name, surname, position, wage, bonus):
-        super().__init__(name, surname, position, wage, bonus)
-
-    def get_full_name(self):
-        return self.name + ' ' + self.surname
-
-    def get_total_income(self):
-        return self._income["wage"] + self._income["bonus"]
-
-p = Position('Misha', 'Mishutka', 'QA', '100000', '20000')
-print(p.get_full_name(), p.get_total_income())
+cell = Cell(24)
+cell_2 = Cell(2)
+print(cell + cell_2)
+print(cell - cell_2)
+print(cell / cell_2)
+print(cell * cell_2)
+print(cell.make_order(7))
