1c60ec62199a5b7e44885dceb204d67d4dc76915
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-10 08:14:21 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,22 +1,32 @@
 #1
 #!/usr/bin/python
 
-from time import sleep
-class TrafficLight:
-    __color=["красный","желтый","зеленый"]
-    def running(self):
-        i=0
-        while i!=3:
-            print(TrafficLight.__color[i])
-            if i==0:
-                sleep(7)
-            elif i==1:
-                sleep(2)
-            elif i==2:
-                sleep(3)
+class Matrix:
+    def __init__(self, my_list):
+        self.my_list = my_list
 
-       
- i+=1
+    def __str__(self):
+        for row in self.my_list:
+            for i in row:
+                print(f"{i:4}", end="")
+            print()
+        return ''
 
-a=TrafficLight()
-a.running()
+    '''2 варианта реализации __str__'''
+
+    # def __str__(self):
+    #     return '\n'.join(map(str, self.my_list))
+
+    # def __str__(self):
+    #     return str('\n'.join(['\t'.join([str(el) for el in i]) for i in self.my_list]))
+
+    def __add__(self, other):
+        for i in range(len(self.my_list)):
+            for i_2 in range(len(other.my_list[i])):
+                self.my_list[i][i_2] = self.my_list[i][i_2] + other.my_list[i][i_2]
+        return Matrix.__str__(self)
+
+
+m = Matrix([[-1, 0, 1], [-1, 0, 1], [0, 1, -1], [1, 1, -1]])
+new_m = Matrix([[-2, 0, 2], [-2, 0, 2], [0, 2, -2], [2, 2, -7]])
+print(m.__add__(new_m))
