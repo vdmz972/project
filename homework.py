34991bb8785a6a3363243e17ca4e95cc2bd93520
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-10 08:14:57 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,21 +1,43 @@
 #2
 
-class Road:
-    def __init__(self,length,width):
-        self.length=length
-        self.width=width
-        self.weigth=25
-        self.height=5
-
-    def raschet_mass(self):
-        mass_asph=self.length*self.weigth*self.weigth*self.height/1000
-        print(f"Для покрытия нужно\n{round(mass_asph)}")
-
-r=Road(5000,20)
-r.raschet_mass()
+from abc import ABC, abstractmethod
 
 
+class Clothes(ABC):
 
+    def __init__(self, param):
+        self.param = param
+
+    @property
+    def consumption(self):
+        return f'Сумма затраченной ткани равна: {self.param / 6.5 + 0.5 + 2 * self.param + 0.3 :.2f}'
+
+    @abstractmethod
+    def abstract(self):
+        return 'Smth vary abstract'
+
+
+class Coat(Clothes):
+    def consumption(self):
+        return f'Для пошива пальто нужно: {self.param / 6.5 + 0.5 :.2f} ткани'
+
+    def abstract(self):
+        return 'Smth vary abstract second'
+
+
+class Costume(Clothes):
+    def consumption(self):
+        return f'Для пошива костюма нужно: {2 * self.param + 0.3 :.2f} ткани'
+
+    def abstract(self):
+        pass
+
+
+coat = Coat(400)
+costume = Costume(55)
+print(coat.consumption)
+print(costume.consumption())
+print(coat.abstract())
 
 
 
