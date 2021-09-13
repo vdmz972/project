1dc84096def743b1419d732c9d2e1a394330f3b1
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-13 11:25:18 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,43 +1,22 @@
 #2
 
-from abc import ABC, abstractmethod
+class DivisionByNull:
+    def __init__(self, divider, denominator):
+        self.divider = divider
+        self.denominator = denominator
+
+    @staticmethod
+    def divide_by_null(divider, denominator):
+        try:
+            return (divider / denominator)
+        except:
+            return (f"Деление на ноль недопустимо")
 
 
-class Clothes(ABC):
-
-    def __init__(self, param):
-        self.param = param
-
-    @property
-    def consumption(self):
-        return f'Сумма затраченной ткани равна: {self.param / 6.5 + 0.5 + 2 * self.param + 0.3 :.2f}'
-
-    @abstractmethod
-    def abstract(self):
-        return 'Smth vary abstract'
-
-
-class Coat(Clothes):
-    def consumption(self):
-        return f'Для пошива пальто нужно: {self.param / 6.5 + 0.5 :.2f} ткани'
-
-    def abstract(self):
-        return 'Smth vary abstract second'
-
-
-class Costume(Clothes):
-    def consumption(self):
-        return f'Для пошива костюма нужно: {2 * self.param + 0.3 :.2f} ткани'
-
-    def abstract(self):
-        pass
-
-
-coat = Coat(400)
-costume = Costume(55)
-print(coat.consumption)
-print(costume.consumption())
-print(coat.abstract())
+div = DivisionByNull(10, 100)
+print(DivisionByNull.divide_by_null(10, 0))
+print(DivisionByNull.divide_by_null(10, 0.1))
+print(div.divide_by_null(100, 0))
 
 
 
