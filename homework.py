523e4dec7fb8c280ae38435107d05728e124a6eb
vdmz972 <61136476+vdmz972@users.noreply.github.com>
2021-09-13 11:39:02 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,29 +1,22 @@
 #5
-class Stationery:
-    def __init__(self, title):
-        self.title = title
-
-    def draw(self):
-        return f'Запуск отрисовки'
-
-
-class Pen(Stationery):
-    def draw(self):
-        return f'Запуск отрисовки {self.title}'
-
-
-class Pencil(Stationery):
-    def draw(self):
-        return f'Запуск отрисовки {self.title}'
-
-
-class Handle(Stationery):
-    def draw(self):
-        return f'Запуск отрисовки {self.title}'
-
-pen = Pen('ручкой')
-print(pen.draw())
-pencil = Pencil('карандашем')
-print(pencil.draw())
-handle = Handle('маркером')
-print(handle.draw())
+class OfficeEquipment:
+    name: str = None
+    weight: float = float()
+    __price: float = None
+    def __init__(self, name, weight, price):
+        self.name   = name
+        self.weight = weight
+        self.price  = price
+ 
+    @property
+    def price(self):
+        return self.__price
+    @price.setter
+    def price(self, price):
+        try:
+            self.__price = float(price)
+        except ValueError:
+            print(f"price {price} is not a number")
+            self.__price = None
+    def __str__(self):
+        return f'{self.name} {self.price} {self.weight}'
